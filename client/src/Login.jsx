ec262eadbdd370e4c1fe8ec44ed7f887583cb8cb
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-09-05 14:19:24 +0530

    commit

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -57,7 +57,7 @@ function Login() {
         </button>
       </form>
       <p>Don't an account?</p>
-      <Link to="/register"></Link>
+      <Link to="/register">Signup</Link>
     </div>
   );
 }
