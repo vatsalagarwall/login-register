1eb76a61784c34bb2d3850da7924a1bbf49cf9cf
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-11-07 15:56:00 +0530

    saved

--- server/index.js
+++ server/index.js
@@ -9,6 +9,11 @@ app.use(cors())
 
 mongoose.connect("mongodb://127.0.0.1:27017/login-register-employees")
 
+
+app.get("/", (req, res) => {
+
+})
+
 app.post("/login", (req, res) => {
     const { email, password } = req.body
     EmployeeModel.findOne({ email: email })
